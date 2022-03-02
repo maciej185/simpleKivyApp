6c8e20415ff05d98b66645e713ad9c5f15d0759e
Maciej <maclis124@gmail.com>
2022-03-02 21:49:17 +0100

    Starting with float layout

--- kivi/apka.py
+++ kivi/apka.py
@@ -4,45 +4,20 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput    
 from kivy.uix.button import Button
 import kivy
-from pkg_resources import PkgResourcesDeprecationWarning
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 class KivyApp(App):
     def build(self):
         return MyGrid()
 
-class MyGrid(GridLayout):
-    def __init__(self, **kwargs):
-        super(MyGrid, self).__init__(**kwargs)
-   
-        self.rows = 2
+class MyGrid(Widget):
+    imie = ObjectProperty(None)
+    pass
 
-        self.frame = GridLayout()   #gorny frame
-        self.frame.cols = 2
-
-        self.frame1 = GridLayout()  #frame z labelka i polem tekstowym
-        self.frame1.rows = 2
-
-        self.tekst = Label(text = "Podaj swoje imię i nazwisko")
-        self.frame1.add_widget(self.tekst)
-
-        self.pole_tekstowe = TextInput()
-        self.frame1.add_widget(self.pole_tekstowe)
-
-        self.frame.add_widget(self.frame1)
-        self.okno = Label(text = " ")
-        self.frame.add_widget(self.okno)
-        self.add_widget(self.frame)
-
-        self.click = Button(text = "Submit")  
-        self.click.bind(on_press = self.pressed)
-        self.add_widget(self.click)
-
-
-    # obsluga zdazenia klikniecia w przycisk
-    def pressed(self, instance):
-        imie = self.pole_tekstowe.text
-        self.pole_tekstowe.text = ""   #czysczenie pola tekstowego
-        self.okno.text = imie    #printowanie inputu w labelce w prawym gornym rogu
+    def pressed(self):
+        self.imie.text = "" #czyszcenie okna z inputem
+        
 
 if __name__ == "__main__":
     KivyApp().run()
