87367eb93bebdce8a959f9423d0ff559ba9e3d1d
Maciej <maclis124@gmail.com>
2022-03-03 20:10:16 +0100

    Dodanie opcji rysowania i czyszczenia ekreanu

--- kivi/apka.py
+++ kivi/apka.py
@@ -1,15 +1,14 @@
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput    
-from kivy.uix.button import Button
+from kivy.core.window import Window   
 import kivy
 from kivy.uix.widget import Widget
-from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
 from kivy.properties import ListProperty
-from kivy.graphics import Rectangle
+from kivy.graphics import Ellipse, Line
 from kivy.graphics import Color
+
+#making the window white
+Window.clearcolor = (1,1,1,1)
 
 class KivyApp(App):
     def build(self):
@@ -17,31 +16,49 @@ class KivyApp(App):
 
 class MyGrid(Widget):
 
-    color = ListProperty((0.5, 0.5, 0.5, 1))
+    color = ListProperty((1, 1, 1, 1))
 
     btn = ObjectProperty(None)
     tekst = ObjectProperty(None)
     
-    def pressed(self):
+    def pressed(self, canvass):
+        global Window
         self.tekst1 = self.tekst
         self.btn.text = ""
+        canvass.clear()
+
     def lifted(self):
         self.btn.text = self.tekst1
+    
+    def increase(self, canvass):
+        canvass.d += 1
+
+    def decrease(self, canvass):
+        canvass.d -= 1
 
 class Touch(Widget):
 
-    color = ListProperty((0.5, 0.5, 0.5, 1))
+    color = ListProperty((1, 1, 1, 1))
+    def __init__(self, **kwargs):
+        super(Touch, self).__init__(**kwargs)
+        self.d = 5
 
-    #btn = ObjectProperty(None)
     def on_touch_down(self, touch):
         print("mouse down ", touch)
-        #self.btn.opacity = 0.5
+        x = touch.pos[0]
+        y = touch.pos[1]
+        self.canvas.add(Color(rgba=(0,0,0,1)))
+        self.canvas.add(Ellipse(segments = 5, pos = (x - self.d/2,y-self.d/2), size = (self.d,self.d)))
+        self.canvas.ask_update()
     def on_touch_up(self, touch):
         print("mouse up ", touch)
-        #self.btn.opacity = 1
+        
     def on_touch_move(self, touch):
+        x = touch.pos[0]
+        y = touch.pos[1]
+        self.canvas.add(Color(rgba=(0,0,0,1)))
+        self.canvas.add(Ellipse(segments = 5, pos = (x - self.d/2,y - self.d/2), size = (self.d,self.d)))
         print("mouse move ", touch)
     
-
 if __name__ == "__main__":
     KivyApp().run()
