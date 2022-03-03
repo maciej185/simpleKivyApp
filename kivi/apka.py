efe9d75fe65eaac7ea183a480637c04442f380da
Maciej <maclis124@gmail.com>
2022-03-03 11:47:25 +0100

    Proste zmiany

--- kivi/apka.py
+++ kivi/apka.py
@@ -5,19 +5,30 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 import kivy
 from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
+from kivy.graphics import Rectangle
+from kivy.graphics import Color
 
 class KivyApp(App):
     def build(self):
         return MyGrid()
 
 class MyGrid(Widget):
-    imie = ObjectProperty(None)
     pass
 
-    def pressed(self):
-        self.imie.text = "" #czyszcenie okna z inputem
-        
+class Touch(Widget):
+
+    #btn = ObjectProperty(None)
+    def on_touch_down(self, touch):
+        print("mouse down ", touch)
+        #self.btn.opacity = 0.5
+    def on_touch_up(self, touch):
+        print("mouse up ", touch)
+        #self.btn.opacity = 1
+    def on_touch_move(self, touch):
+        print("mouse move ", touch)
+    
 
 if __name__ == "__main__":
     KivyApp().run()
